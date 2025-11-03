015e7149698f4e93595647fc10418e440a26c4fe
Shadow X <djedaietn@gmail.com>
2025-11-03 14:41:45 +0000

    use this for تعبةوقود https://www.flaticon.com/free-icon/gas-station_48

--- src/components/dashboard/top-services.tsx
+++ src/components/dashboard/top-services.tsx
@@ -10,7 +10,7 @@ const services = [
   { id: 'battery', name: 'بطارية وشحن', name_en: 'Battery & Charging', icon: 'https://cdn-icons-png.flaticon.com/512/3071/3071441.png' },
   { id: 'tires', name: 'فحص الإطارات', name_en: 'Tire Check', icon: 'https://cdn-icons-png.flaticon.com/512/10755/10755050.png' },
   { id: 'electronic', name: 'فحص إلكتروني', name_en: 'Electronic Check', icon: 'https://cdn-icons-png.flaticon.com/512/3202/3202926.png' },
-  { id: 'fuel', name: 'تعبئة وقود', name_en: 'Fuel Refill', icon: 'https://cdn-icons-png.flaticon.com/512/2704/2704319.png' },
+  { id: 'fuel', name: 'تعبئة وقود', name_en: 'Fuel Refill', icon: 'https://cdn-icons-png.flaticon.com/512/481/481233.png' },
   { id: 'tech_support', name: 'دعم تقني', name_en: 'Tech Support', icon: 'https://cdn-icons-png.flaticon.com/512/2618/2618521.png' },
 ];
 
