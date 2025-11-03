17de683cefcaebc18cdbb574bdcff4e5b4aed785
Shadow X <djedaietn@gmail.com>
2025-11-03 12:48:27 +0000

    this is the correct link https://imgur.com/5CxFHKm

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,7 +9,7 @@ export function Header() {
       <div className="container flex h-16 max-w-screen-2xl items-center">
         <Link href="/" className="flex items-center gap-2 font-bold text-lg text-primary">
           <div className="relative w-8 h-8">
-            <Image src="https://i.imgur.com/3Z4QjB8.png" alt="Mechasos Logo" fill className="p-1 bg-white rounded-md object-contain" />
+            <Image src="https://i.imgur.com/5CxFHKm.png" alt="Mechasos Logo" fill className="p-1 bg-white rounded-md object-contain" />
           </div>
           <div className="flex flex-col">
             <span className="font-headline text-xl leading-none" lang="en">Mechasos</span>
